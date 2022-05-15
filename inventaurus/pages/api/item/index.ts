0dd78f45eeaeceff497cb24644c9783c40150560
vfreve <44242537+VicoF@users.noreply.github.com>
2022-05-15 11:01:50 -0400

    basic rest api

---
+++ inventaurus/pages/api/item/index.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma';
+
+export default async function handler(req : NextApiRequest, res:NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      const items = await prisma.item.findMany();
+      res.status(200).json(items);
+      return;
+    case 'POST':
+      // TODO validation
+      const item = req.body;
+      prisma.item.upsert(item)
+      res.status(200);
+      return;
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
